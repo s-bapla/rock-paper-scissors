a5d075c1bc1d53d7d0b38dec4fcc91d693acb181
Sunny Bapla <98226491+s-bapla@users.noreply.github.com>
2024-04-02 14:17:31 -0700

    finish ui

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
+scissors.addEventListener("click", () =>
+  playRound("scissors", getComputerChoice())
+);
+paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
+
 function getComputerChoice() {
   let rand_int = Math.floor(Math.random() * 3);
   switch (rand_int) {
@@ -9,102 +18,74 @@ function getComputerChoice() {
       return "scissors";
   }
 }
-
+let compScore = 0;
+let playerScore = 0;
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
+  const div = document.querySelector(".results");
+  const h1 = document.querySelector(".score");
 
   if (playerSelection === "rock") {
     switch (computerSelection) {
       case "rock":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, tie`
-        );
-        return "tie";
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, tie`;
+        compScore += 1;
+        playerScore += 1;
+        break;
       case "paper":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, lose`
-        );
-        return "lose";
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, lose`;
+        compScore += 1;
+        break;
       case "scissors":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, win`
-        );
-        return "win";
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, win`;
+        playerScore += 1;
+        break;
     }
   } else if (playerSelection === "paper") {
     switch (computerSelection) {
       case "rock":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, win`
-        );
-        return "win";
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, win`;
+        playerScore += 1;
+        break;
       case "paper":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, tie`
-        );
-        return "tie";
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, tie`;
+        compScore += 1;
+        playerScore += 1;
+        break;
       case "scissors":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, lose`
-        );
-        return "lose";
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, lose`;
+        compScore += 1;
+        break;
     }
   } else {
     switch (computerSelection) {
       case "rock":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, lose`
-        );
-        return "lose";
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, lose`;
+        compScore += 1;
+        break;
       case "paper":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, win`
-        );
-        return "win";
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, win`;
+        playerScore += 1;
+        break;
       case "scissors":
-        console.log(
-          `player choice: ${playerSelection}, computer choice: ${computerSelection}, tie`
-        );
-        return "tie";
-    }
-  }
-}
-
-function playGame() {
-  let compScore = 0;
-  let playerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("choose rock paper or scissors: ");
-    const computerSelection = getComputerChoice();
-    result = playRound(playerSelection, computerSelection);
-
-    switch (result) {
-      case "win":
-        playerScore += 1;
-        break;
-      case "lose":
+        div.textContent = `player choice: ${playerSelection}, computer choice: ${computerSelection}, tie`;
         compScore += 1;
-        break;
-      case "tie":
         playerScore += 1;
-        compScore += 1;
         break;
     }
   }
-  if (playerScore > compScore) {
-    console.log(
-      `You win. your score: ${playerScore} computer score: ${compScore}`
-    );
-    return `You win. your score: ${playerScore} computer score: ${compScore}`;
-  } else if (compScore > playerScore) {
-    console.log(
-      `You lose. your score: ${playerScore} computer score: ${compScore}`
-    );
-    return `You lose. your score: ${playerScore} computer score: ${compScore}`;
-  } else {
-    console.log(
-      `Tie. your score: ${playerScore} computer score: ${compScore}`
-    );
-    return `Tie. your score: ${playerScore} computer score: ${compScore}`;
+
+  h1.textContent = `computer: ${compScore} - player: ${playerScore}`;
+  
+  if (playerScore === 5 || compScore === 5) {
+    if (playerScore > compScore) {
+      h1.textContent = `You win. your score: ${playerScore} computer score: ${compScore}`;
+    } else if (compScore > playerScore) {
+      h1.textContent = `You lose. your score: ${playerScore} computer score: ${compScore}`;
+    } else {
+      h1.textContent = `Tie. your score: ${playerScore} computer score: ${compScore}`;
+    }
+    playerScore = 0;
+    compScore = 0;
   }
 }
